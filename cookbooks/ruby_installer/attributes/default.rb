a660b7264ed27e6f2ee39fd1aa03e16908fee78e
Nick Smith <1hockeyplayer+github@gmail.com>
2013-08-23 03:38:10 -0700

    Update default.rb
    Update to the latest version of Ruby and Rubygems.

--- cookbooks/ruby_installer/attributes/default.rb
+++ cookbooks/ruby_installer/attributes/default.rb
@@ -2,9 +2,9 @@ default[:ruby_installer][:method] = 'package'
 default[:ruby_installer][:package_name] = true
 default[:ruby_installer][:rubygem_package] = true
 default[:ruby_installer][:rubydev_package] = true
-default[:ruby_installer][:source_version] = '1.9.3-p194'
+default[:ruby_installer][:source_version] = '2.0.0-p247'
 default[:ruby_installer][:source_install_dir] = '/usr/local'
-default[:ruby_installer][:source_rubygems_version] = '1.8.24'
+default[:ruby_installer][:source_rubygems_version] = '2.0.7'
 default[:ruby_installer][:source_rubygems_force] = false
 default[:ruby_installer][:source_package_dependencies] = case node.platform_family
 when 'debian'
